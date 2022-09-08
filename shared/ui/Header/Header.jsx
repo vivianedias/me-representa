7ffa0576a75aa0f179c84641fd8aaa3f6f501995
Kaê Uchôa <kae.uchoa.s@gmail.com>
2022-09-08 17:02:31 -0300

    feat: adiciona traducao para o header (#6)

--- shared/ui/Header/Header.jsx
+++ shared/ui/Header/Header.jsx
@@ -6,25 +6,28 @@ import logo from "/public/imgs/logotipo.png";
 import Image from "next/image";
 import { Button, IconButton, SlideFade } from "@chakra-ui/react";
 import styles from "./styles.module.css";
+import { useTranslation } from "react-i18next";
 
-const MenuIcon = () => (
+const MenuIcon = () => {
+  return (
   <>
     <svg
       fill="#fd006c"
       width="32px"
       viewBox="0 0 20 20"
       xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
     >
-      <title>Menu</title>
       <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
     </svg>
   </>
-);
+)};
 
 const Header = () => {
-  const [show, setShow] = useState(true);
-  const handleToggle = () => setShow(!show);
-
+  const [isVisible, setIsVisible] = useState(true);
+  const handleToggle = () => setIsVisible(!isVisible);
+  const { t } = useTranslation('translation', {keyPrefix: "header"}) 
+  const btnText = t("navbar.menu", {estado: isVisible ? t("navbar.menuFechar") : t("navbar.menuAbrir")})
   const routeChange = () => {
     console.log("route change");
   };
@@ -33,11 +36,11 @@ const Header = () => {
     <header className={styles.container}>
       <div className={styles.logoContainer}>
         <NextLink href="/">
-          <Image id="logo" src={logo} alt="Logotipo MeRepresenta" />
+          <Image id="logo" src={logo} alt={t("imgDescricao")} />
         </NextLink>
         <span className={styles.menuBtn}>
           <IconButton
-            aria-label="Search database"
+            aria-label={btnText}
             icon={<MenuIcon />}
             variant="ghost"
             colorScheme="pink"
@@ -46,29 +49,26 @@ const Header = () => {
         </span>
       </div>
       <nav className={styles.navBar}>
-        <SlideFade in={show}>
+        <SlideFade in={isVisible}>
           <ul>
             <li>
-              <NextLink href="/">Sobre</NextLink>
+              <NextLink href="/">{t("navbar.sobre")}</NextLink>
             </li>
             <li>
-              <NextLink href="/login">Candidata/o</NextLink>
+              <NextLink href="#">{t("navbar.voluntaria")}</NextLink>
             </li>
             <li>
-              <NextLink href="#">Voluntária/o</NextLink>
+              <NextLink href="#">{t("navbar.eleitora")}</NextLink>
             </li>
             <li>
-              <NextLink href="#">Eleitor/a</NextLink>
+              <NextLink href="/cadastro/candidato-pautas">{t("navbar.pautas")}</NextLink>
             </li>
             <li>
-              <NextLink href="/cadastro/candidato-pautas">Pautas</NextLink>
-            </li>
-            <li>
-              <NextLink href="#">Perguntas?</NextLink>
+              <NextLink href="#">{t("navbar.perguntas")}</NextLink>
             </li>
             <li>
               <Button onClick={routeChange} colorScheme="pink">
-                Sou Candidata/o
+              {t("navbar.candidata")}
               </Button>
             </li>
           </ul>
