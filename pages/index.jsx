ebbe02fc27e448f16559b3f0f6a3193c44e7e602
Kaê Uchôa <kae.uchoa.s@gmail.com>
2022-09-08 18:17:08 -0300

    feat: adiciona assets, componente e tradução para o footer (#7)
    * feat: adiciona imagens para o footer
    
    * feat: adiciona footer

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,14 @@
+// import Home from './home/Home'
+import '../shared/locales/i18n'
+import Footer from '/shared/ui/Footer/Footer'
+import Header from '/shared/ui/Header/Header'
+
 export default function App() {
   return (
     <div>
+      <Header />
+      {/* <Home /> */}
+      <Footer />
     </div>
   )
 }
