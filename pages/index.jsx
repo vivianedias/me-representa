81a042e9dcdf2a224517519bd37e7e99aec64215
Viviane Dias <vivianedias@protonmail.com>
2022-09-12 18:01:23 -0300

    fix: moves home comp to shared ui, and leaves index as home

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,6 @@
-import Home from './home/Home'
-import '../shared/locales/i18n'
+import Home from "../shared/ui/Home/Home";
+import "../shared/locales/i18n";
 
 export default function App() {
-  return (
-    <div>
-      <Home />
-    </div>
-  )
+  return <Home />;
 }
