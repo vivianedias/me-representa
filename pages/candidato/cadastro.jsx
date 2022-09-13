e53955a27ee6efe0f714ff187d246a8b7dc37352
Viviane Dias <vivianedias@protonmail.com>
2022-09-13 19:58:13 -0300

    feat: adds photo upload to aws s3

--- pages/candidato/cadastro.jsx
+++ pages/candidato/cadastro.jsx
@@ -1,6 +1,7 @@
 import "../../shared/locales/i18n";
 import { useSession } from "next-auth/react";
 import { Form, Field } from "react-final-form";
+import { useTranslation } from "react-i18next";
 import {
   Box,
   FormControl,
@@ -12,28 +13,41 @@ import {
   Heading,
   FormLabel,
   FormErrorMessage,
+  Text,
+  FormHelperText,
+  VStack,
+  Image,
+  Spinner,
+  HStack,
+  Center,
 } from "@chakra-ui/react";
 import { FaUserAlt } from "react-icons/fa";
 import validations from "../../utils/validations";
-import { useTranslation } from "react-i18next";
+import useUploadS3 from "../../shared/hooks/useUploadS3";
 
 export default function CadastroCandidato({ data }) {
   const { status, data: session } = useSession({
     required: true,
     onUnauthenticated() {
-      // signIn();
-      console.log("logoff");
+      signIn();
     },
   });
+  const { imageUrl, uploadS3, isLoading } = useUploadS3();
   const CFaUserAlt = chakra(FaUserAlt);
   const { t } = useTranslation("translation", { keyPrefix: "cadastro" });
-  const { required } = validations(t);
+  const { required, email, composeValidators, cpf, length } = validations(t);
 
-  // if (status === "loading") {
-  //   return <p>Loading...</p>;
-  // }
+  const handleFileInput = (e, onChange) => {
+    const file = e.target.files[0];
+    uploadS3(file);
+    onChange(e);
+  };
 
   const onSubmit = (data) => console.log({ data });
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
 
   return (
     <Stack
@@ -45,7 +59,7 @@ export default function CadastroCandidato({ data }) {
       backgroundColor="whiteAlpha.900"
       boxShadow="md"
     >
-      <Box as="section" bgColor="white" minW={{ base: "90%", md: "768px" }}>
+      <Box as="section" bgColor="white" w={{ base: "90%", md: "768px" }}>
         <Heading as="h1" size="2xl" align="center">
           Candidato
         </Heading>
@@ -53,12 +67,16 @@ export default function CadastroCandidato({ data }) {
           onSubmit={onSubmit}
           initialValues={{
             email: session?.user?.email,
+            image: session?.user?.image,
           }}
           render={({ handleSubmit, submitting, submitError }) => {
             return (
               <Box as="form" onSubmit={handleSubmit} my={10}>
                 <Stack spacing={4} align="center">
-                  <Field name="email" validate={required}>
+                  <Field
+                    name="email"
+                    validate={composeValidators(required, email)}
+                  >
                     {({ input, meta }) => {
                       return (
                         <FormControl isInvalid={meta.error && meta.touched}>
@@ -78,15 +96,15 @@ export default function CadastroCandidato({ data }) {
                       );
                     }}
                   </Field>
-                  <Field name="cpf" validate={required}>
+                  <Field
+                    name="cpf"
+                    validate={composeValidators(required, cpf, length(11))}
+                  >
                     {({ input, meta }) => {
                       return (
                         <FormControl isInvalid={meta.error && meta.touched}>
                           <FormLabel>{t("cpf.label")}</FormLabel>
                           <InputGroup>
-                            <InputLeftElement pointerEvents="none">
-                              <CFaUserAlt color="gray.300" />
-                            </InputLeftElement>
                             <Input
                               {...input}
                               type="cpf"
@@ -95,6 +113,47 @@ export default function CadastroCandidato({ data }) {
                           </InputGroup>
                           <FormErrorMessage>{meta.error}</FormErrorMessage>
                         </FormControl>
+                      );
+                    }}
+                  </Field>
+                  <Field name="image" validate={required}>
+                    {({ input, meta }) => {
+                      return (
+                        <Stack spacing={4}>
+                          <Text align="center" fontSize="20px" fontWeight={700}>
+                            {t("image.label")}
+                          </Text>
+                          <FormControl isInvalid={meta.error && meta.touched}>
+                            <HStack spacing={10} align="centers">
+                              <>
+                                <Center boxSize="300px" position="absolute">
+                                  {isLoading ? <Spinner size="xl" /> : null}
+                                </Center>
+                                <Image
+                                  borderRadius="full"
+                                  boxSize="300px"
+                                  alt="Profile photo selected by the candidate"
+                                  src={imageUrl}
+                                  fallbackSrc="https://via.placeholder.com/300"
+                                />
+                              </>
+                              <VStack>
+                                <Input
+                                  {...input}
+                                  onChange={(e) =>
+                                    handleFileInput(e, input.onChange)
+                                  }
+                                  type="file"
+                                  placeholder={t("image.placeholder")}
+                                />
+                                <FormHelperText>
+                                  {t("image.helperText")}
+                                </FormHelperText>
+                              </VStack>
+                            </HStack>
+                            <FormErrorMessage>{meta.error}</FormErrorMessage>
+                          </FormControl>
+                        </Stack>
                       );
                     }}
                   </Field>
---
+++ shared/hooks/useUploadS3.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { PutObjectCommand } from "@aws-sdk/client-s3";
+import { S3Client } from "@aws-sdk/client-s3";
+
+const REGION = process.env.NEXT_PUBLIC_AWS_S3_BUCKET_REGION;
+const ACCESS_KEY_ID = process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID;
+const SECRET_ACCESS_KEY = process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY;
+const BUCKET_NAME = process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME;
+const BUCKET_URL = process.env.NEXT_PUBLIC_AWS_S3_BUCKET_URL;
+
+const s3Client = new S3Client({
+  region: REGION,
+  credentials: {
+    accessKeyId: ACCESS_KEY_ID,
+    secretAccessKey: SECRET_ACCESS_KEY,
+  },
+});
+
+const useUploadS3 = () => {
+  const [imageUrl, setImageUrl] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const uploadS3 = async (file) => {
+    setIsLoading(true);
+
+    const bucketParams = {
+      ACL: "public-read",
+      Bucket: BUCKET_NAME,
+      Key: file.name,
+      Body: file,
+    };
+
+    try {
+      await s3Client.send(new PutObjectCommand(bucketParams));
+      setImageUrl(`${BUCKET_URL}/${file.name}`);
+      setIsLoading(false);
+    } catch (e) {
+      console.error(e);
+      setIsLoading(false);
+    }
+  };
+
+  return {
+    imageUrl,
+    uploadS3,
+    isLoading,
+  };
+};
+
+export default useUploadS3;
