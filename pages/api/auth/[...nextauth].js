c3c6caba446124776a8879e5766c3d5c0f2f146a
Viviane Dias <vivianedias@protonmail.com>
2022-09-13 10:22:26 -0300

    feat: adds pretty email template to confirmation email

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,11 @@
 import NextAuth from 'next-auth'
 import EmailProvider from "next-auth/providers/email";
 import TwitterProvider from "next-auth/providers/twitter";
-import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import sgMail from "@sendgrid/mail";
 import clientPromise from "../../../lib/mongodb";
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const options = {
   providers: [
@@ -12,15 +14,35 @@ const options = {
         port: 465,
         host: "smtp.sendgrid.net",
         secure: true,
-        auth: {
-          user: "apikey",
-          pass: process.env.SENDGRID_API_KEY,
-        },
         tls: {
           rejectUnauthorized: false,
         },
       },
       from: process.env.EMAIL_FROM,
+      async sendVerificationRequest({
+        identifier: email,
+        url,
+        provider: { from },
+      }) {
+        const msg = {
+          to: email,
+          from,
+          templateId: process.env.SENDGRID_CONFIRMATION_TEMPLATE_ID,
+          dynamicTemplateData: {
+            link: url,
+          },
+        };
+
+        try {
+          await sgMail.send(msg);
+        } catch (error) {
+          console.error(error);
+
+          if (error.response) {
+            console.error(error.response.body);
+          }
+        }
+      },
     }),
     TwitterProvider({
       clientId: process.env.TWITTER_CLIENT_ID,
