50fbe740c866acf47f08f190dc5f856b0adac576
Kaê Uchôa <kae.uchoa.s@gmail.com>
2022-09-14 18:43:24 -0300

    feat: cria componente para Pergunta (#12)

--- shared/locales/i18n.js
+++ shared/locales/i18n.js
@@ -7,6 +7,10 @@ i18n.use(initReactI18next).init({
   resources: {
     "pt-BR": {
       translation: {
+        global: {
+          contador: "Pauta {{current}} de {{max}}",
+          posicionamento: "Qual o seu posicionamento?",
+        },
         login: {
           loading: "Carregando",
           redirect: "Redirecionando",
