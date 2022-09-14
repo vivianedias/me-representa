23098694a42ccdc59e5d437a94059001af546cbd
Viviane Dias <vivianedias@protonmail.com>
2022-09-14 17:24:54 -0300

    feat: change from users to candidates

---
+++ pages/api/candidate/register.js
@@ -0,0 +1,20 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import clientPromise from "../../../lib/mongodb";
+import { ObjectId } from "mongodb";
+
+export async function updateCandidate(req, res) {
+  try {
+    const { id } = req.query;
+    const parsedId = ObjectId(id);
+
+    const client = await clientPromise;
+    const db = client.db("merepresenta");
+    const findResult = await db
+      .collection("candidates")
+      .updateOne({ _id: parsedId }, { $set: req.body });
+    return findResult;
+  } catch (e) {
+    console.log(e);
+    return [];
+  }
+}
