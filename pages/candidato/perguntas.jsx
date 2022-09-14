3b429f358a272fc59a59c99cf1480841c7c3b4a7
Kaê Uchôa <kae.uchoa.s@gmail.com>
2022-09-14 18:47:19 -0300

    refactor: atualiza a pergunta 1 para usar novo comp

--- pages/candidato/perguntas.jsx
+++ pages/candidato/perguntas.jsx
@@ -4,6 +4,7 @@ import { signIn, useSession } from "next-auth/react"
 import { useTranslation } from "react-i18next"
 import { Form } from "react-final-form"
 import { RadioGroup, Radio } from "/shared/ui/Radio/Radio"
+import { Pergunta } from "/shared/ui/Pergunta/Pergunta"
 import "/shared/locales/i18n.js"
 
 const Perguntas = ({ data }) => {
@@ -21,7 +22,9 @@ const Perguntas = ({ data }) => {
 
   return (
     <Container as="section">
-      <Heading as="h1" marginY={6} textAlign="center">{t("titulo")}</Heading>
+      <Heading as="h1" marginY={6} textAlign="center">
+        {t("titulo")}
+      </Heading>
       <section>
         <LGBT />
       </section>
@@ -32,9 +35,6 @@ const Perguntas = ({ data }) => {
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 const LGBT = ({ currentCount = 1, maxCount = 22 }) => {
-  const { t: tModule } = useTranslation("translation", {
-    keyPrefix: "candidato.perguntas",
-  })
   const { t } = useTranslation("translation", {
     keyPrefix: "candidato.perguntas.LGBTQ",
   })
@@ -43,45 +43,26 @@ const LGBT = ({ currentCount = 1, maxCount = 22 }) => {
     window.alert(JSON.stringify(values, 0, 2))
   }
 
-  const idCirurgia = "cirurgia"
   return (
     <section>
-      <Heading as="h3" size="lg" marginY={3}>{t('titulo')}</Heading>
+      <Heading as="h3" size="lg" marginY={3}>
+        {t("titulo")}
+      </Heading>
       <Form
         onSubmit={onSubmit}
         render={({ handleSubmit, form, submitting, pristine, values }) => (
           <form onSubmit={handleSubmit}>
-            <div>
-              <Text as="strong">{tModule("contador", {current: currentCount, max: maxCount })}</Text>
-              <Heading as="h3" size="lg" id={idCirurgia}>
-                {t("cirurgia.label")}
-              </Heading>
-              <Box as="p" marginY={3}>
-                {t("cirurgia.pergunta")}
-              </Box>
-              <p>{t("cirurgia.info")}</p>
-              <Box marginY={3} as="strong" display="block">
-                {tModule("posicionamento")}
-              </Box>
-              <RadioGroup ariaLabelledBy={idCirurgia}>
-                <Box marginY={3}>
-                  <Radio
-                    name={idCirurgia}
-                    value="favor"
-                    label={t("cirurgia.labelAFavor")}
-                    style={{ width: "100%" }}
-                  />
-                </Box>
-                <Box marginY={3}>
-                  <Radio
-                    name={idCirurgia}
-                    value="contra"
-                    label={t("cirurgia.labelContra")}
-                    style={{ width: "100%" }}
-                  />
-                </Box>
-              </RadioGroup>
-            </div>
+            <Pergunta
+              contador={{ currentCount: currentCount, maxCount: maxCount }}
+              pergunta={{
+                id: "cirurgia",
+                label: t("cirurgia.label"),
+                texto: t("cirurgia.pergunta"),
+                info: t("cirurgia.info"),
+                labelFavor: t("cirurgia.labelAFavor"),
+                labelContra: t("cirurgia.labelContra"),
+              }}
+            />
             {/* <div>
               <h3>{t("2.label")}</h3>
               <p>{t("2.pergunta")}</p>
