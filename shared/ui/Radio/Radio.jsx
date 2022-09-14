04f3cdb1f09b8646b1810f4834205f8941d80dac
Kaê Uchôa <kae.uchoa.s@gmail.com>
2022-09-14 17:02:24 -0300

    style: adiciona comentário para RadioGroup

--- shared/ui/Radio/Radio.jsx
+++ shared/ui/Radio/Radio.jsx
@@ -3,6 +3,10 @@ import PropTypes from "prop-types"
 import { Button } from "@chakra-ui/react"
 import { FormSpy, useField } from "react-final-form"
 
+/* 
+  * Para ser usado como wrapper dos Radio. 
+  * Adiciona acessibilidade para os componentes Radio através da id usada no aria-labelledby 
+*/
 export const RadioGroup = ({ ariaLabelledBy, children, ...rest }) => {
   return (
     <span aria-labelledby={ariaLabelledBy} role="radiogroup" {...rest}>
