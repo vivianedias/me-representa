de6c404e12f1edb7cbfc9f584b46bd48436c087c
Kaê Uchôa <kae.uchoa.s@gmail.com>
2022-09-14 16:59:14 -0300

    feat: cria o botão Radio e RadioGroup pra usar com react-final-form

---
+++ shared/ui/Radio/Radio.jsx
@@ -0,0 +1,63 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { Button } from "@chakra-ui/react"
+import { FormSpy, useField } from "react-final-form"
+
+export const RadioGroup = ({ ariaLabelledBy, children, ...rest }) => {
+  return (
+    <span aria-labelledby={ariaLabelledBy} role="radiogroup" {...rest}>
+      {children}
+    </span>
+  )
+}
+
+RadioGroup.propTypes = {
+  ariaLabelledBy: PropTypes.string.isRequired
+}
+
+export const Radio = ({
+  name,
+  value,
+  label,
+  style,
+  className,
+  colorScheme = "blue",
+}) => {
+  const field = useField(name, {
+    type: "radio",
+    value: value,
+    initialValue: "",
+  })
+  const toggle = () => field.input.onChange(field.input.value)
+
+  return (
+    <FormSpy subscription={{ values: true }}>
+      {({ values }) => (
+        <>
+          <input type="radio" {...field.input} style={{ display: "none" }} />
+          <Button
+            variant={values[name] === value ? "solid" : "outline"}
+            colorScheme={colorScheme}
+            onClick={toggle}
+            style={style}
+            className={className}
+            maxW="100%"
+            boxSizing="content-box"
+            role="radio"
+          >
+            {label}
+          </Button>
+        </>
+      )}
+    </FormSpy>
+  )
+}
+
+Radio.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  style: PropTypes.object,
+  className: PropTypes.string,
+  colorScheme: PropTypes.string, // Baseado no chakra colorScheme
+}
