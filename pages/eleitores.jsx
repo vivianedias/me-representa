8d0568bb9871f28d936c7299c7c081f4a680485a
Camila Cardoso <caamila.cardoso.l@gmail.com>
2022-09-15 11:43:00 -0300

    Adds Eleitores filter

--- pages/eleitores.jsx
+++ pages/eleitores.jsx
@@ -1,19 +1,190 @@
-import fetchClient from '../utils/apiClient'
-import '../shared/locales/i18n'
+import fetchClient from "../utils/apiClient";
+import "../shared/locales/i18n";
+import { Form } from "react-final-form";
+import Head from "next/head";
+import {
+	Box,
+	Button,
+	Checkbox,
+	CheckboxGroup,
+	Heading,
+	Select,
+	Stack,
+} from "@chakra-ui/react";
+import { useTranslation } from "react-i18next";
 
-export default function Dashboard({ data }) {
-  return (
-    <>
-      <div>Dashboard</div>
-      {data.map(i => (<p key={i._id}>{i.email}</p>))}
-    </>
-  )
+function Identity({ t }) {
+	return (
+		<Stack spacing={2}>
+			<Heading as="h2" size="sm" align="left">
+				{t("filters.identity.label")}
+			</Heading>
+			<CheckboxGroup
+				colorScheme="yellow"
+				defaultValue={["black", "indigenous", "women", "lgbt"]}
+			>
+				<Stack direction={"row"} wrap="wrap" align="start">
+					<Checkbox value="black">{t("filters.identity.black")}</Checkbox>
+					<Checkbox value="indigenous">
+						{t("filters.identity.indigenous")}
+					</Checkbox>
+					<Checkbox value="women">{t("filters.identity.women")}</Checkbox>
+					<Checkbox value="lgbt">{t("filters.identity.lgbt")}</Checkbox>
+				</Stack>
+			</CheckboxGroup>
+		</Stack>
+	);
+}
+
+function LGBT({ t }) {
+	return (
+		<Stack spacing={2}>
+			<Heading as="h2" size="sm" align="left">
+				{t("filters.lgbt.label")}
+			</Heading>
+			<CheckboxGroup
+				colorScheme="yellow"
+				defaultValue={["lesbian", "gay", "bisexual", "trans"]}
+			>
+				<Stack spacing={4} direction={"row"}>
+					<Checkbox value="lesbian">{t("filters.lgbt.lesbian")}</Checkbox>
+					<Checkbox value="gay">{t("filters.lgbt.gay")}</Checkbox>
+					<Checkbox value="bisexual">{t("filters.lgbt.bisexual")}</Checkbox>
+					<Checkbox value="trans">{t("filters.lgbt.trans")}</Checkbox>
+				</Stack>
+			</CheckboxGroup>
+		</Stack>
+	);
+}
+
+function Parties({ t }) {
+	return (
+		<Heading as="h2" size="sm" align="left">
+			{t("filters.parties.label")}
+		</Heading>
+	);
+}
+
+function Priorities({ t }) {
+	return (
+		<Stack spacing={2}>
+			<Heading as="h2" size="sm" align="left">
+				{t("filters.priorities.label")}
+			</Heading>
+			<CheckboxGroup
+				colorScheme="yellow"
+				defaultValue={["gender", "security", "race"]}
+			>
+				<Stack spacing={4} direction={"column"}>
+					<Checkbox value="corruption">
+						{t("filters.priorities.corruption")}
+					</Checkbox>
+					<Checkbox value="gender">{t("filters.priorities.gender")}</Checkbox>
+					<Checkbox value="drugs">{t("filters.priorities.drugs")}</Checkbox>
+					<Checkbox value="security">
+						{t("filters.priorities.security")}
+					</Checkbox>
+					<Checkbox value="migration">
+						{t("filters.priorities.migration")}
+					</Checkbox>
+					<Checkbox value="race">{t("filters.priorities.race")}</Checkbox>
+					<Checkbox value="traditionalPopulationEnvironment">
+						{t("filters.priorities.traditionalPopulationEnvironment")}
+					</Checkbox>
+					<Checkbox value="healthEducationWork">
+						{t("filters.priorities.healthEducationWork")}
+					</Checkbox>
+				</Stack>
+			</CheckboxGroup>
+		</Stack>
+	);
+}
+
+function State({ t }) {
+	return (
+		<Stack spacing={2}>
+			<Heading as="h2" size="sm" align="left">
+				{t("filters.state.label")}
+			</Heading>
+			<Select placeholder={t("filters.state.label")}>
+				<option value="SP">SP</option>
+				<option value="RJ">RJ</option>
+				<option value="ES">ES</option>
+			</Select>
+		</Stack>
+	);
+}
+
+export default function EleitoresDashboard({ data }) {
+	const { t } = useTranslation("translation", { keyPrefix: "eleitores" });
+
+	const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+	const onSubmit = async (data) => {
+		await sleep(300);
+		console.log({ data });
+	};
+
+	return (
+		<>
+			<Head>
+				<title>{t("title")}</title>
+				<meta property="og:title" content={t("title")} key="title" />
+			</Head>
+			<Stack
+				flexDir="column"
+				mb="2"
+				justifyContent="center"
+				alignItems="center"
+				p="1rem"
+				backgroundColor="whiteAlpha.900"
+				boxShadow="md"
+			>
+				<Form
+					onSubmit={onSubmit}
+					render={({ handleSubmit, submitting, submitError }) => {
+						return (
+							<Box
+								as="form"
+								onSubmit={handleSubmit}
+								bgColor="white"
+								w={{ base: "85vw", md: "768px" }}
+							>
+								<Stack spacing={8}>
+									<Heading as="h1" size="md" align="left">
+										{t("filters.heading")}
+									</Heading>
+
+									<Identity t={t} />
+									<LGBT t={t} />
+									<Parties t={t} />
+									<Priorities t={t} />
+									<State t={t} />
+
+									<Button
+										type="submit"
+										isLoading={submitting}
+										loadingText="Filtrando"
+										variant="solid"
+										colorScheme="pink"
+										size="md"
+									>
+										{t("filters.button")}
+									</Button>
+								</Stack>
+							</Box>
+						);
+					}}
+				/>
+			</Stack>
+		</>
+	);
 }
 
 export async function getServerSideProps(req, res) {
-  // Fetch data from external API
-  const data = await fetchClient('/api/users')
+	// Fetch data from external API
+	// const data = await fetchClient("/api/users");
 
-  // Pass data to the page via props
-  return { props: { data } }
+	// Pass data to the page via props
+	return { props: { data: [] } };
 }
--- shared/locales/i18n.js
+++ shared/locales/i18n.js
@@ -2,176 +2,215 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
 i18n.use(initReactI18next).init({
-  fallbackLng: "pt-BR",
-  lng: "pt-BR",
-  resources: {
-    "pt-BR": {
-      translation: {
-        login: {
-          loading: "Carregando",
-          redirect: "Redirecionando",
-          title: "Login",
-          twitter: {
-            button: "Entrar com Twitter",
-          },
-          email: {
-            placeholder: "endereço de e-mail",
-            validation: {
-              required: "Campo obrigatório",
-            },
-            verificationEmail: {
-              title: "Cheque seu e-mail!",
-              body: "Um link mágico para login acaba de ser enviado!",
-              error:
-                "Houve um erro ao enviar o link. Tente novamente mais tarde.",
-            },
-            button: "Entrar com Email",
-          },
-        },
-        header: {
-          imgDescricao: "Logotipo do MeRepresenta",
-          navbar: {
-            home: "Home",
-            voluntaria: "Voluntária/o",
-            eleitora: "Eleitor/a",
-            perguntas: "Perguntas?",
-            candidata: "Sou Candidata/o",
-            menu: "Clique para {{estado}} o menu",
-            menuAbrir: "abrir",
-            menuFechar: "fechar",
-            auth: {
-              profile: "Editar perfil",
-              logout: "Sair",
-              pautas: "Pautas",
-            },
-          },
-        },
-        footer: {
-          sobre: {
-            titulo: "Sobre",
-            quemSomos: "Quem somos",
-            impacto: "Impacto",
-            transparencia: "Transparência",
-          },
-          participar: {
-            titulo: "Participar",
-            candidato: "Candidato",
-            eleitor: "Eleitor",
-            voluntario: "Voluntário",
-          },
-          financiamento: {
-            titulo: "Financiamento",
-            iniciativa: "uma iniciativa de",
-            imgsAlt: {
-              ciudadania: "Ciudadania Inteligente",
-              altec: "Uma iniciativa de Altec",
-              undef: "Undef",
-              avina: "Avina",
-              omidyar: "Omidyar",
-            },
-          },
-          apoio: {
-            titulo: "Apoio",
-            imgsAlt: {
-              zoly: "Saiba Mais Zoly",
-              mattos: "Mattos Filho",
-              silveira: "Silveira Andrade",
-              casa1: "Casa 1",
-              tiniguimaraes: "Tini e Guimarães",
-              dataLabel: "Data Label",
-            },
-          },
-          direitos: {
-            titulo: "#Todos os direitos reservados a #MeRepresenta 2022",
-          },
-        },
-        home: {
-          ajudeEleitores: {
-            titulo: "Ajude seus eleitores chegarem até você!",
-            querAparecer:
-              "Quer aparecer para as eleitoras e eleitores que se preocupam com as mesmas pautas que você? Cadastre-se e traga mais visibilidade para sua candidatura.",
-            btnSouCandidato: "Sou candidata/o",
-            btnSouVoluntario: "Quero ser voluntária/o",
-            btnSouEleitor: "Sou eleitor/a",
-            imgDescricao:
-              "Eleitores diversos com balões de fala dizendo 'voto!' e '#me representa'",
-          },
-          conheca: {
-            titulo: "Conheça o #MeRepresenta",
-            oMeRepresenta:
-              "O #MeRepresenta é uma união de coletivos formados por mulheres, pessoas negras e LGBT+. Nosso objetivo é diminuir a distância entre eleitoras/es e candidatas/os comprometidas/os com os direitos sociais, civis e políticos da população.",
-            aNovaPlataforma:
-              "A nova plataforma #MeRepresenta Eleições 2020 foi elaborada com a participação de 16 organizações da sociedade civil. Veja abaixo quem está com a gente!",
-          },
-          pautas: {
-            titulo: "Entenda as #pautas em debate",
-            genero: "#Gênero",
-            lgbt: "#LGBT+",
-            raca: "#Raça",
-            povos: "#Povos Tradicionais",
-            politicasSociais: "#Políticas Sociais",
-            segurancaPublica: "#Segurança Pública",
-            drogas: "#Drogas",
-            comunicacao: "#Comunicação",
-            demoracracia: "#Democracia",
-            meioAmbiente: "#Meio Ambiente",
-            imgsAlt: {
-              raca: "Pessoas negras marcham com punho erguido e sorrindo",
-              povos: "Indigenas com os corpos pintados em protesto",
-              lgbt: "Bandeira do movimento LGBT+ balançada em frente a grande público em passeata",
-            },
-          },
-          facaParte: {
-            titulo: "Faça parte do #MeRepresenta",
-            btnVoluntario: "Quero ser voluntario",
-            btnSaberMais: "Quero saber mais",
-            imgDescricao:
-              "Ilustração com 3 pessoas com balões de fala dizendo '#me representa'",
-          },
-          quemFez: {
-            titulo: "Quem fez isso possível?",
-            imgsAlt: {
-              mulheresNegras:
-                "Logo com a frase 'mulheres negras decidem' em maiusculas",
-              blogueirasNegras:
-                "Logo com uma mulher negra a esquerda em fundo amarelo e o título 'Blogueiras Negras'",
-              redeFeminista:
-                "Logo com uma arte em linhas finas e o título 'Rede Feminista de Juristas'",
-              cidadaniaInteligente:
-                "Logo com uma arte de uma lâmpada e pontos coloridos com o título 'Cidadania Inteligente'",
-              voteLGBT: "Logo com o título '#VoteLGBT'",
-            },
-          },
-          parcerias: {
-            titulo: "Parcerias",
-            imgsAlt: {
-              agblt:
-                "Logotipo da Associação Brasileira de Gays, Lésbicas, Bissexuais, Travestis, Transexuais e Intersexos",
-              aliancaNacionalLGBTI: "Logotipo da Aliança Nacional LGBTI+",
-              apoinme:
-                "Logotipo da Articulação dos Povos Indígenas do Nordeste, Minas Gerais e Espírito Santo (APOINME)",
-              cfemea: "Logotipo do Centro Feminista de Estudos e Assessoria",
-              conaq:
-                "Logotipo da Coordenação Nacional de Articulação de Quilombos",
-              gn: "Logotipo da orgnização Gênero e Número",
-              instutoSouDaPaz: "Logotipo do Instituto Sou da Paz",
-              intervozes: "Logotipo da Intervozes",
-              ittc: "Logotitpo do Instituto Terra, Trabalho e Cidadania - ITTC",
-              mst: "Logotipo do Movimento dos Trabalhadores Rurais Sem Terra",
-              pbpd: "Logotipo da Plataforma Brasileira de Política de Drogas (PBPD)",
-              sistemaPolitico:
-                "Logotipo da Plataforma pela reforma do sistema político",
-              redeJusticaCriminal: "Logotipo da Rede Justiça Criminal",
-              azMina: "Logotipo do instituto AzMina",
-              movimentoTransparencia:
-                "Logotipo do Movimento Transparência Partidária",
-              monabot: "Logotipo da M0na Bot",
-            },
-          },
-        },
-      },
-    },
-  },
+	fallbackLng: "pt-BR",
+	lng: "pt-BR",
+	resources: {
+		"pt-BR": {
+			translation: {
+				eleitores: {
+					title: "Eleitores",
+					filters: {
+						heading: "FILTROS",
+						identity: {
+							label: "Identitários",
+							black: "Negros",
+							indigenous: "Indígenas",
+							women: "Mulheres",
+							lgbt: "LGBT",
+						},
+						lgbt: {
+							label: "LGBT",
+							lesbian: "Lésbica",
+							gay: "Gay",
+							bisexual: "Bissexual",
+							trans: "Trans",
+						},
+						parties: {
+							label: "Partidos",
+						},
+						priorities: {
+							label: "Prioridades",
+							corruption: "Corrupção",
+							gender: "Gênero",
+							drugs: "Drogas",
+							security: "Segurança",
+							migration: "Imigrantes",
+							race: "Raça",
+							traditionalPopulationEnvironment:
+								"Povos Tradicionais e Meio Ambiente",
+							healthEducationWork: "Saúde, Educação e Trabalho",
+						},
+						state: {
+							label: "Estado",
+						},
+						button: "Filtrar",
+					},
+				},
+				login: {
+					loading: "Carregando",
+					redirect: "Redirecionando",
+					title: "Login",
+					twitter: {
+						button: "Entrar com Twitter",
+					},
+					email: {
+						placeholder: "endereço de e-mail",
+						validation: {
+							required: "Campo obrigatório",
+						},
+						verificationEmail: {
+							title: "Cheque seu e-mail!",
+							body: "Um link mágico para login acaba de ser enviado!",
+							error:
+								"Houve um erro ao enviar o link. Tente novamente mais tarde.",
+						},
+						button: "Entrar com Email",
+					},
+				},
+				header: {
+					imgDescricao: "Logotipo do MeRepresenta",
+					navbar: {
+						home: "Home",
+						voluntaria: "Voluntária/o",
+						eleitora: "Eleitor/a",
+						perguntas: "Perguntas?",
+						candidata: "Sou Candidata/o",
+						menu: "Clique para {{estado}} o menu",
+						menuAbrir: "abrir",
+						menuFechar: "fechar",
+						auth: {
+							profile: "Editar perfil",
+							logout: "Sair",
+							pautas: "Pautas",
+						},
+					},
+				},
+				footer: {
+					sobre: {
+						titulo: "Sobre",
+						quemSomos: "Quem somos",
+						impacto: "Impacto",
+						transparencia: "Transparência",
+					},
+					participar: {
+						titulo: "Participar",
+						candidato: "Candidato",
+						eleitor: "Eleitor",
+						voluntario: "Voluntário",
+					},
+					financiamento: {
+						titulo: "Financiamento",
+						iniciativa: "uma iniciativa de",
+						imgsAlt: {
+							ciudadania: "Ciudadania Inteligente",
+							altec: "Uma iniciativa de Altec",
+							undef: "Undef",
+							avina: "Avina",
+							omidyar: "Omidyar",
+						},
+					},
+					apoio: {
+						titulo: "Apoio",
+						imgsAlt: {
+							zoly: "Saiba Mais Zoly",
+							mattos: "Mattos Filho",
+							silveira: "Silveira Andrade",
+							casa1: "Casa 1",
+							tiniguimaraes: "Tini e Guimarães",
+							dataLabel: "Data Label",
+						},
+					},
+					direitos: {
+						titulo: "#Todos os direitos reservados a #MeRepresenta 2022",
+					},
+				},
+				home: {
+					ajudeEleitores: {
+						titulo: "Ajude seus eleitores chegarem até você!",
+						querAparecer:
+							"Quer aparecer para as eleitoras e eleitores que se preocupam com as mesmas pautas que você? Cadastre-se e traga mais visibilidade para sua candidatura.",
+						btnSouCandidato: "Sou candidata/o",
+						btnSouVoluntario: "Quero ser voluntária/o",
+						btnSouEleitor: "Sou eleitor/a",
+						imgDescricao:
+							"Eleitores diversos com balões de fala dizendo 'voto!' e '#me representa'",
+					},
+					conheca: {
+						titulo: "Conheça o #MeRepresenta",
+						oMeRepresenta:
+							"O #MeRepresenta é uma união de coletivos formados por mulheres, pessoas negras e LGBT+. Nosso objetivo é diminuir a distância entre eleitoras/es e candidatas/os comprometidas/os com os direitos sociais, civis e políticos da população.",
+						aNovaPlataforma:
+							"A nova plataforma #MeRepresenta Eleições 2020 foi elaborada com a participação de 16 organizações da sociedade civil. Veja abaixo quem está com a gente!",
+					},
+					pautas: {
+						titulo: "Entenda as #pautas em debate",
+						genero: "#Gênero",
+						lgbt: "#LGBT+",
+						raca: "#Raça",
+						povos: "#Povos Tradicionais",
+						politicasSociais: "#Políticas Sociais",
+						segurancaPublica: "#Segurança Pública",
+						drogas: "#Drogas",
+						comunicacao: "#Comunicação",
+						demoracracia: "#Democracia",
+						meioAmbiente: "#Meio Ambiente",
+						imgsAlt: {
+							raca: "Pessoas negras marcham com punho erguido e sorrindo",
+							povos: "Indigenas com os corpos pintados em protesto",
+							lgbt: "Bandeira do movimento LGBT+ balançada em frente a grande público em passeata",
+						},
+					},
+					facaParte: {
+						titulo: "Faça parte do #MeRepresenta",
+						btnVoluntario: "Quero ser voluntario",
+						btnSaberMais: "Quero saber mais",
+						imgDescricao:
+							"Ilustração com 3 pessoas com balões de fala dizendo '#me representa'",
+					},
+					quemFez: {
+						titulo: "Quem fez isso possível?",
+						imgsAlt: {
+							mulheresNegras:
+								"Logo com a frase 'mulheres negras decidem' em maiusculas",
+							blogueirasNegras:
+								"Logo com uma mulher negra a esquerda em fundo amarelo e o título 'Blogueiras Negras'",
+							redeFeminista:
+								"Logo com uma arte em linhas finas e o título 'Rede Feminista de Juristas'",
+							cidadaniaInteligente:
+								"Logo com uma arte de uma lâmpada e pontos coloridos com o título 'Cidadania Inteligente'",
+							voteLGBT: "Logo com o título '#VoteLGBT'",
+						},
+					},
+					parcerias: {
+						titulo: "Parcerias",
+						imgsAlt: {
+							agblt:
+								"Logotipo da Associação Brasileira de Gays, Lésbicas, Bissexuais, Travestis, Transexuais e Intersexos",
+							aliancaNacionalLGBTI: "Logotipo da Aliança Nacional LGBTI+",
+							apoinme:
+								"Logotipo da Articulação dos Povos Indígenas do Nordeste, Minas Gerais e Espírito Santo (APOINME)",
+							cfemea: "Logotipo do Centro Feminista de Estudos e Assessoria",
+							conaq:
+								"Logotipo da Coordenação Nacional de Articulação de Quilombos",
+							gn: "Logotipo da orgnização Gênero e Número",
+							instutoSouDaPaz: "Logotipo do Instituto Sou da Paz",
+							intervozes: "Logotipo da Intervozes",
+							ittc: "Logotitpo do Instituto Terra, Trabalho e Cidadania - ITTC",
+							mst: "Logotipo do Movimento dos Trabalhadores Rurais Sem Terra",
+							pbpd: "Logotipo da Plataforma Brasileira de Política de Drogas (PBPD)",
+							sistemaPolitico:
+								"Logotipo da Plataforma pela reforma do sistema político",
+							redeJusticaCriminal: "Logotipo da Rede Justiça Criminal",
+							azMina: "Logotipo do instituto AzMina",
+							movimentoTransparencia:
+								"Logotipo do Movimento Transparência Partidária",
+							monabot: "Logotipo da M0na Bot",
+						},
+					},
+				},
+			},
+		},
+	},
 });
 
 export default i18n;
