7564c3b0a29a6acb664cf58e8e5b456584106f83
Camila Cardoso <caamila.cardoso.l@gmail.com>
2022-09-15 16:12:42 -0300

    WIP

--- pages/eleitores.jsx
+++ pages/eleitores.jsx
@@ -1,6 +1,6 @@
 import fetchClient from "../utils/apiClient";
 import "../shared/locales/i18n";
-import { Form } from "react-final-form";
+import { Form, Field } from "react-final-form";
 import Head from "next/head";
 import {
 	Box,
@@ -8,10 +8,19 @@ import {
 	Checkbox,
 	CheckboxGroup,
 	Heading,
+	Input,
 	Select,
 	Stack,
 } from "@chakra-ui/react";
 import { useTranslation } from "react-i18next";
+
+function CustomCheckbox(props) {
+	return (
+		<Field name={props.name} type="checkbox" value={props.value}>
+			{({ input }) => <Checkbox {...input}>{props.label}</Checkbox>}
+		</Field>
+	);
+}
 
 function Identity({ t }) {
 	return (
@@ -19,17 +28,28 @@ function Identity({ t }) {
 			<Heading as="h2" size="sm" align="left">
 				{t("filters.identity.label")}
 			</Heading>
-			<CheckboxGroup
-				colorScheme="yellow"
-				defaultValue={["black", "indigenous", "women", "lgbt"]}
-			>
+			<CheckboxGroup colorScheme="yellow">
 				<Stack direction={"row"} wrap="wrap" align="start">
-					<Checkbox value="black">{t("filters.identity.black")}</Checkbox>
-					<Checkbox value="indigenous">
-						{t("filters.identity.indigenous")}
-					</Checkbox>
-					<Checkbox value="women">{t("filters.identity.women")}</Checkbox>
-					<Checkbox value="lgbt">{t("filters.identity.lgbt")}</Checkbox>
+					<CustomCheckbox
+						name="identity"
+						value="black"
+						label={t("filters.identity.black")}
+					/>
+					<CustomCheckbox
+						name="identity"
+						value="indigenous"
+						label={t("filters.identity.indigenous")}
+					/>
+					<CustomCheckbox
+						name="identity"
+						value="women"
+						label={t("filters.identity.women")}
+					/>
+					<CustomCheckbox
+						name="identity"
+						value="lgbt"
+						label={t("filters.identity.lgbt")}
+					/>
 				</Stack>
 			</CheckboxGroup>
 		</Stack>
@@ -42,15 +62,28 @@ function LGBT({ t }) {
 			<Heading as="h2" size="sm" align="left">
 				{t("filters.lgbt.label")}
 			</Heading>
-			<CheckboxGroup
-				colorScheme="yellow"
-				defaultValue={["lesbian", "gay", "bisexual", "trans"]}
-			>
+			<CheckboxGroup colorScheme="yellow">
 				<Stack spacing={4} direction={"row"}>
-					<Checkbox value="lesbian">{t("filters.lgbt.lesbian")}</Checkbox>
-					<Checkbox value="gay">{t("filters.lgbt.gay")}</Checkbox>
-					<Checkbox value="bisexual">{t("filters.lgbt.bisexual")}</Checkbox>
-					<Checkbox value="trans">{t("filters.lgbt.trans")}</Checkbox>
+					<CustomCheckbox
+						name="lgbt"
+						value="lesbian"
+						label={t("filters.lgbt.lesbian")}
+					/>
+					<CustomCheckbox
+						name="lgbt"
+						value="gay"
+						label={t("filters.lgbt.gay")}
+					/>
+					<CustomCheckbox
+						name="lgbt"
+						value="bisexual"
+						label={t("filters.lgbt.bisexual")}
+					/>
+					<CustomCheckbox
+						name="lgbt"
+						value="trans"
+						label={t("filters.lgbt.trans")}
+					/>
 				</Stack>
 			</CheckboxGroup>
 		</Stack>
@@ -71,29 +104,48 @@ function Priorities({ t }) {
 			<Heading as="h2" size="sm" align="left">
 				{t("filters.priorities.label")}
 			</Heading>
-			<CheckboxGroup
-				colorScheme="yellow"
-				defaultValue={["gender", "security", "race"]}
-			>
+			<CheckboxGroup colorScheme="yellow">
 				<Stack spacing={4} direction={"column"}>
-					<Checkbox value="corruption">
-						{t("filters.priorities.corruption")}
-					</Checkbox>
-					<Checkbox value="gender">{t("filters.priorities.gender")}</Checkbox>
-					<Checkbox value="drugs">{t("filters.priorities.drugs")}</Checkbox>
-					<Checkbox value="security">
-						{t("filters.priorities.security")}
-					</Checkbox>
-					<Checkbox value="migration">
-						{t("filters.priorities.migration")}
-					</Checkbox>
-					<Checkbox value="race">{t("filters.priorities.race")}</Checkbox>
-					<Checkbox value="traditionalPopulationEnvironment">
-						{t("filters.priorities.traditionalPopulationEnvironment")}
-					</Checkbox>
-					<Checkbox value="healthEducationWork">
-						{t("filters.priorities.healthEducationWork")}
-					</Checkbox>
+					<CustomCheckbox
+						name="priorities"
+						value="corruption"
+						label={t("filters.priorities.corruption")}
+					/>
+					<CustomCheckbox
+						name="priorities"
+						value="gender"
+						label={t("filters.priorities.gender")}
+					/>
+					<CustomCheckbox
+						name="priorities"
+						value="drugs"
+						label={t("filters.priorities.drugs")}
+					/>
+					<CustomCheckbox
+						name="priorities"
+						value="security"
+						label={t("filters.priorities.security")}
+					/>
+					<CustomCheckbox
+						name="priorities"
+						value="migration"
+						label={t("filters.priorities.migration")}
+					/>
+					<CustomCheckbox
+						name="priorities"
+						value="race"
+						label={t("filters.priorities.race")}
+					/>
+					<CustomCheckbox
+						name="priorities"
+						value="traditionalPopulationEnvironment"
+						label={t("filters.priorities.traditionalPopulationEnvironment")}
+					/>
+					<CustomCheckbox
+						name="priorities"
+						value="healthEducationWork"
+						label={t("filters.priorities.healthEducationWork")}
+					/>
 				</Stack>
 			</CheckboxGroup>
 		</Stack>
@@ -142,7 +194,8 @@ export default function EleitoresDashboard({ data }) {
 			>
 				<Form
 					onSubmit={onSubmit}
-					render={({ handleSubmit, submitting, submitError }) => {
+					render={({ handleSubmit, submitting, submitError, ...rest }) => {
+						console.log(rest.values);
 						return (
 							<Box
 								as="form"
