d166a5458538579bb09c770e18e78502024c5f07
Kaê Uchôa <kae.uchoa.s@gmail.com>
2022-09-17 12:57:07 -0300

    feat: adiciona estilos para wizard

--- shared/ui/Wizard/Wizard.jsx
+++ shared/ui/Wizard/Wizard.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react"
 import PropTypes from "prop-types"
+import { v4 as uuid } from "uuid"
 import { Field, Form } from "react-final-form"
-import { Button } from "@chakra-ui/react"
 import { useTranslation } from "react-i18next"
-import "/shared/locales/i18n";
+
+import { Box, Button } from "@chakra-ui/react"
+import { FaCaretLeft, FaCaretRight } from "react-icons/fa"
+
+import styles from "./styles.module.css"
+import "/shared/locales/i18n"
 
 export const Wizard = ({ initialValues, onSubmit, children }) => {
   const { t } = useTranslation("translation", { keyPrefix: "wizard" })
@@ -11,15 +16,16 @@ export const Wizard = ({ initialValues, onSubmit, children }) => {
     page: 0,
     values: initialValues || {},
   })
-  const activePage = React.Children.toArray(children)[state.page]
+  const childrenArray = React.Children.toArray(children)
+  const activePage = childrenArray[state.page]
   const isLastPage = state.page === React.Children.count(children) - 1
 
-  const handleSubmit = (values) => {
+  const onSubmitHandler = (values) => {
     const isLastPage = state.page === React.Children.count(children) - 1
     if (isLastPage) {
       return onSubmit(values)
     }
-    next(values)
+    onNextHandler(values)
   }
 
   const pageValidation = (values) => {
@@ -32,30 +38,42 @@ export const Wizard = ({ initialValues, onSubmit, children }) => {
     return errors
   }
 
-  const next = (values) => {
+  const onNextHandler = (values) => {
     setState((state) => ({
       page: Math.min(state.page + 1, children.length - 1),
       values,
     }))
   }
 
-  const previous = () => {
+  const onPreviousHandler = () => {
     setState((state) => ({ ...state, page: Math.max(state.page - 1, 0) }))
   }
 
   const renderPrevious = () => {
-    return state.page > 0 && <Button onClick={previous}>{t("anterior")}</Button>
+    return (
+      state.page > 0 && (
+        <Button leftIcon={<FaCaretLeft />} onClick={onPreviousHandler}>
+          {t("anterior")}
+        </Button>
+      )
+    )
   }
 
   const renderNext = () => {
-    return !isLastPage && <Button type="submit">{t("proximo")}</Button>
+    return (
+      !isLastPage && (
+        <Button rightIcon={<FaCaretRight />} type="submit">
+          {t("proximo")}
+        </Button>
+      )
+    )
   }
 
   const renderSubmit = (isSubmitting, hasValidationErrors) => {
     return (
       isLastPage && (
         <Button
-          colorScheme="pink"
+          colorScheme="blue"
           type="submit"
           disabled={isSubmitting || hasValidationErrors}
         >
@@ -66,27 +84,51 @@ export const Wizard = ({ initialValues, onSubmit, children }) => {
   }
 
   return (
-    <Form
-      initialValues={state.values}
-      validate={pageValidation}
-      onSubmit={handleSubmit}
-    >
-      {({ handleSubmit, submitting, hasValidationErrors }) => (
-        <form onSubmit={handleSubmit}>
-          {activePage}
-          <div>
-            {renderPrevious()}
-            {renderNext()}
-            {renderSubmit(submitting, hasValidationErrors)}
-          </div>
-        </form>
-      )}
-    </Form>
+    <div className={styles.wizardContainer}>
+      <Wizard.Steps currentPage={state.page} childrenArray={childrenArray} />
+      <div className={styles.formContainer}>
+        <Form
+          initialValues={state.values}
+          validate={pageValidation}
+          onSubmit={onSubmitHandler}
+        >
+          {({ handleSubmit, submitting, hasValidationErrors }) => (
+            <form onSubmit={handleSubmit}>
+              {activePage}
+              <div
+                className={`${styles.footer} ${
+                  state.page === 0 ? styles.continueFooter : ""
+                }`}
+              >
+                {renderPrevious()}
+                {renderNext()}
+                {renderSubmit(submitting, hasValidationErrors)}
+              </div>
+            </form>
+          )}
+        </Form>
+      </div>
+    </div>
   )
 }
 
 Wizard.propTypes = {
   onSubmit: PropTypes.func.isRequired,
+}
+
+Wizard.Steps = function Steps({ currentPage, childrenArray }) {
+  return (
+    <Box className={styles.stepsContainer}>
+      {childrenArray.map((_, index) => (
+        <span
+          key={uuid()}
+          className={`${styles.step} ${
+            index <= currentPage ? styles.activeStep : ""
+          }`}
+        />
+      ))}
+    </Box>
+  )
 }
 
 Wizard.Page = function Page({ children }) {
