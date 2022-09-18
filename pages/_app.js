0f352efb0ea2c623f8db4752154cd686dd776fdf
Camila Cardoso <caamila.cardoso.l@gmail.com>
2022-09-18 15:15:43 -0300

    formatting

--- pages/_app.js
+++ pages/_app.js
@@ -7,34 +7,34 @@ import Footer from "../shared/ui/Footer/Footer";
 import "../styles/globals.css";
 
 const theme = {
-	colors: {
-		yellow: {
-			500: "#ECC94B",
-			600: "#ECC94B",
-		},
-	},
+  colors: {
+    yellow: {
+      500: "#ECC94B",
+      600: "#ECC94B",
+    },
+  },
 };
 
 function MyApp({ Component, pageProps }) {
-	return (
-		<SessionProvider session={pageProps.session}>
-			<ChakraProvider theme={extendTheme(theme)}>
-				<Header />
-				<Flex
-					flexDirection="column"
-					width="100wh"
-					minHeight="calc(100vh - 450px)"
-					backgroundColor="gray.200"
-					justifyContent="center"
-					alignItems="center"
-					as="main"
-				>
-					<Component {...pageProps} />
-				</Flex>
-				<Footer />
-			</ChakraProvider>
-		</SessionProvider>
-	);
+  return (
+    <SessionProvider session={pageProps.session}>
+      <ChakraProvider theme={extendTheme(theme)}>
+        <Header />
+        <Flex
+          flexDirection="column"
+          width="100wh"
+          minHeight="calc(100vh - 450px)"
+          backgroundColor="gray.200"
+          justifyContent="center"
+          alignItems="center"
+          as="main"
+        >
+          <Component {...pageProps} />
+        </Flex>
+        <Footer />
+      </ChakraProvider>
+    </SessionProvider>
+  );
 }
 
 export default MyApp;
