39851bf97e1b28025a46254360fdaa36e2ce1b99
Camila Cardoso <caamila.cardoso.l@gmail.com>
2022-09-18 22:47:09 -0300

    Fixes index

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,6 @@
 import Home from "../shared/ui/Home/Home";
 import "../shared/locales/i18n";
-import { Box } from "@chakra-ui/react";
 
-export default function App({ children }) {
-  return (
-    <Box bgColor="red">
-      {children({
-        input: {
-          onChange: () => "",
-        },
-      })}
-    </Box>
-  );
+export default function App() {
+  return <Home />;
 }
-
-<App>
-  {function children({ input }) {
-    return <Box {...input}>{oi}</Box>;
-  }}
-</App>;
