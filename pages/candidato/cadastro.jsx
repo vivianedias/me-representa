fa4e0191c45fe85dec7cd05659f19af1efd429ee
Viviane Dias <vivianedias@protonmail.com>
2022-09-18 22:51:01 -0300

    feat: creates question page

--- pages/candidato/cadastro.jsx
+++ pages/candidato/cadastro.jsx
@@ -331,7 +331,7 @@ export default function CadastroCandidato(props) {
 
     setInitialValues(
       formatInitialValues({
-        candidate,
+        candidate: newCandidate,
       })
     );
   };
