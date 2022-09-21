1a58f8780b02f9d2c02ec5f86c8d5a96d9035061
Kaê Uchôa <kae.uchoa.s@gmail.com>
2022-09-21 19:41:34 -0300

    teste reactGA em app

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,18 @@
-import "@fontsource/roboto";
-import { SessionProvider } from "next-auth/react";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import { Flex } from "@chakra-ui/react";
-import { SWRConfig } from "swr";
-import Header from "../shared/ui/Header/Header";
-import Footer from "../shared/ui/Footer/Footer";
-import fetcher from "../utils/apiClient";
-import "../styles/globals.css";
+import "@fontsource/roboto"
+import { SessionProvider } from "next-auth/react"
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+import { Flex } from "@chakra-ui/react"
+import { SWRConfig } from "swr"
+import Header from "../shared/ui/Header/Header"
+import Footer from "../shared/ui/Footer/Footer"
+import fetcher from "../utils/apiClient"
+import "../styles/globals.css"
 import ReactGA from 'react-ga';
+import { useEffect } from "react"
+
+const GA_TRACKING_ID = process.env.GA_TRACKING_ID || "G-NB8VZ0Q87J" // remover essa id depois
+
+ReactGA.initialize(GA_TRACKING_ID);
 
 const theme = {
   colors: {
@@ -20,36 +25,40 @@ const theme = {
     heading: `'Roboto', sans-serif`,
     body: `'Roboto', sans-serif`,
   },
-};
-
-const GA_TRACKING_ID = process.env.GA_TRACKING_ID || "4083645711" // remover essa id depois
-ReactGA.initialize(GA_TRACKING_ID)
+}
 
 function MyApp({ Component, pageProps }) {
   const options = {
     fetcher,
-  };
+  }
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
-    <SWRConfig value={options}>
-      <SessionProvider session={pageProps.session}>
-        <ChakraProvider theme={extendTheme(theme)}>
-          <Header />
-          <Flex
-            flexDirection="column"
-            width="100wh"
-            minHeight="calc(100vh - 450px)"
-            backgroundColor="gray.200"
-            justifyContent="center"
-            alignItems="center"
-            as="main"
-          >
-            <Component {...pageProps} />
-          </Flex>
-          <Footer />
-        </ChakraProvider>
-      </SessionProvider>
-    </SWRConfig>
-  );
+    <>
+      <SWRConfig value={options}>
+        <SessionProvider session={pageProps.session}>
+          <ChakraProvider theme={extendTheme(theme)}>
+            <Header />
+            <Flex
+              flexDirection="column"
+              width="100wh"
+              minHeight="calc(100vh - 450px)"
+              backgroundColor="gray.200"
+              justifyContent="center"
+              alignItems="center"
+              as="main"
+            >
+              <Component {...pageProps} />
+            </Flex>
+            <Footer />
+          </ChakraProvider>
+        </SessionProvider>
+      </SWRConfig>
+    </>
+  )
 }
 
-export default MyApp;
+export default MyApp
