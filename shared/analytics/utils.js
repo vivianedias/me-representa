3ff34bf5544a5b3b9bcd3291ede1e1959ea7264e
Viviane Dias <vivianedias@protonmail.com>
2022-09-23 08:47:28 -0300

    debug

--- shared/analytics/utils.js
+++ shared/analytics/utils.js
@@ -1,6 +1,7 @@
 export const GA_TRACKING_ID = process.env.GA_TRACKING_ID;
 
 export const pageview = (url) => {
+  console.log({ GA_TRACKING_ID, url });
   window.gtag("config", GA_TRACKING_ID, {
     page_path: url,
   });
