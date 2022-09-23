8e3cb32ce9fc9cb31df67ecf2ad5ae27a39e72b7
Viviane Dias <vivianedias@protonmail.com>
2022-09-22 22:45:50 -0300

    feat: gets all candidates and politicalParties

--- pages/api/candidates.js
+++ pages/api/candidates.js
@@ -1,4 +1,4 @@
-import clientPromise from "../../../lib/mongodb";
+import clientPromise from "../../lib/mongodb";
 
 export default async function getCandidates(req, res) {
   try {
@@ -6,8 +6,12 @@ export default async function getCandidates(req, res) {
     const db = client.db("merepresenta");
 
     const findResult = await db.collection("candidates").find({}).toArray();
+    const count = await db.collection("candidates").count();
 
-    return res.status(200).json(findResult);
+    return res.status(200).json({
+      candidates: findResult,
+      count,
+    });
   } catch (e) {
     console.log(e);
     return res.status(400);
