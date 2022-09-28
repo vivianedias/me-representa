aef6395254702eeb5f656cbb09095e32c1c6a0ab
Viviane Dias <vivianedias@protonmail.com>
2022-09-28 13:11:35 -0300

    fix: return early if normalized name is falsy

--- utils/normalizeName.js
+++ utils/normalizeName.js
@@ -3,6 +3,9 @@ export default function normalizeName(name) {
   const normalizedNames = names.map((name) => {
     const lowerCaseName = name.toLowerCase();
     let splitName = lowerCaseName.split("");
+
+    if (splitName.length < 1) return name;
+
     splitName[0] = splitName[0].toUpperCase();
     return splitName.join("");
   });
